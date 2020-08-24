82dd0557ebd148809e5a5cd3a4f184260128af9e
unknown <Shaditya2498@gmail.com>
2020-08-24 13:25:51 +0530

    Updates

---
+++ precache-manifest.d668511fccbb5d75d7731ad3dc23cdb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04bda327154c20247daad51723005e5",
+    "url": "/botbuddies/index.html"
+  },
+  {
+    "revision": "269aa89103223bdf45ab",
+    "url": "/botbuddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0132fe69abca977121ba",
+    "url": "/botbuddies/static/css/main.8f647462.chunk.css"
+  },
+  {
+    "revision": "269aa89103223bdf45ab",
+    "url": "/botbuddies/static/js/2.4134dfdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/botbuddies/static/js/2.4134dfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0132fe69abca977121ba",
+    "url": "/botbuddies/static/js/main.79e1ef31.chunk.js"
+  },
+  {
+    "revision": "0aedba9d40bbe666f6ac",
+    "url": "/botbuddies/static/js/runtime-main.821030f9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/botbuddies/static/media/SEGA.12a3db8b.woff"
+  }
+]);
